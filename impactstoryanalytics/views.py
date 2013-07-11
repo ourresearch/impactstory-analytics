5bbad11197ff72b14c3c71035e99228205c6f42a
Jason Priem <jp@jasonpriem.org>
2013-07-10 22:00:20 -0700

    gmail inbox size endpoint works for heather, jason.

--- impactstoryanalytics/views.py
+++ impactstoryanalytics/views.py
@@ -1,5 +1,5 @@
 import requests, os, json, logging
-from impactstoryanalytics import app
+from impactstoryanalytics import app, gmail
 
 from flask import request, abort, make_response, g, redirect, url_for
 from flask import render_template
@@ -15,3 +15,15 @@ logger = logging.getLogger("impactstoryanalytics.views")
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/inbox-count')
+def inbox_count():
+    count = gmail.count_threads_in_inbox(
+        os.getenv("GMAIL_CLIENT_ID"),
+        os.getenv("GMAIL_CLIENT_SECRET"),
+        os.getenv("GMAIL_REFRESH_TOKEN_HEATHER"),
+        os.getenv("GMAIL_ADDRESS_HEATHER")
+    )
+
+    return make_response(str(count))
