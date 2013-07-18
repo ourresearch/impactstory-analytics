2248ddf5da6658c020a9243b369c14416646091f
Jason Priem <jp@jasonpriem.org>
2013-07-17 22:00:24 -0700

    return the two repos' issues as a zipped-together list

--- impactstoryanalytics/widgets.py
+++ impactstoryanalytics/widgets.py
@@ -184,7 +184,8 @@ class Github(Widget):
             line = self.make_line(issues_list)
             lines[repo_name] = line.values()
 
-        return lines
+        zipped = zip(*lines.values())
+        return zipped
 
 
     def get_both_closed_and_open_issues(self, q_url):
