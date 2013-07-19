81996f6da241cd7a9d5b8e09b0f33d59bb0ec778
Heather Piwowar <hpiwowar@gmail.com>
2013-07-19 06:55:18 -0700

    add support for caching
    need to install pylibmc and add MEMCACHIER env vars
    cache.py is from core.  better way to share these files in the future?

---
+++ impactstoryanalytics/cache.py
@@ -0,0 +1,65 @@
+import os
+import pylibmc
+import hashlib
+import logging
+import json
+from cPickle import PicklingError
+
+#from totalimpact.utils import Retry
+
+# set up logging
+logger = logging.getLogger("ti.cache")
+
+class CacheException(Exception):
+    pass
+
+class Cache(object):
+    """ Maintains a cache of URL responses in memcached """
+
+    def _build_hash_key(self, key):
+        json_key = json.dumps(key)
+        hash_key = hashlib.md5(json_key.encode("utf-8")).hexdigest()
+        return hash_key
+
+    def _get_memcached_client(self):
+        mc = pylibmc.Client(
+            servers=[os.environ.get('MEMCACHIER_SERVERS')],
+            username=os.environ.get('MEMCACHIER_USERNAME'),
+            password=os.environ.get('MEMCACHIER_PASSWORD'),
+            binary=True)
+        return mc
+ 
+    def __init__(self, max_cache_age=60*60):  #one hour
+        self.max_cache_age = max_cache_age
+
+
+    def flush_cache(self):
+        #empties the cache
+        mc = self._get_memcached_client()        
+        mc.flush_all()
+
+    #@Retry(3, pylibmc.Error, 0.1)
+    def get_cache_entry(self, key):
+        """ Get an entry from the cache, returns None if not found """
+        mc = self._get_memcached_client()
+        hash_key = self._build_hash_key(key)
+        response = mc.get(hash_key)
+        return response
+
+    #@Retry(3, pylibmc.Error, 0.1)
+    def set_cache_entry(self, key, data):
+        """ Store a cache entry """
+        mc = self._get_memcached_client()
+        hash_key = self._build_hash_key(key)
+        try:
+            set_response = mc.set(hash_key, data, time=self.max_cache_age)
+            if not set_response:
+                raise CacheException("Unable to store into Memcached. Make sure memcached server is running.")
+        except PicklingError:
+            # This happens when trying to cache a thread.lock object, for example.  Just don't cache.
+            logger.debug("In set_cache_entry but got PicklingError")
+            set_response = None
+        return (set_response)
+  
+        
+        
--- impactstoryanalytics/widgets.py
+++ impactstoryanalytics/widgets.py
@@ -7,6 +7,7 @@ import iso8601
 import os
 import logging
 import pytz
+import cache
 
 
 logger = logging.getLogger("impactstoryanalytics.widgets")
@@ -248,12 +249,12 @@ class Github(Widget):
 class Keen(Widget):
 
     total_accounts_query_url = "https://dataclips.heroku.com/brczfyjvdlovipuuukgjselrnilk.json"
-    #active_accounts_query_url = "https://api.keen.io/3.0/projects/51d858213843314922000002/queries/count_unique?api_key=69023dd079bdb913522954c0f9bb010766be7e87a543674f8ee5d3a66e9b127f5ee641546858bf2c260af4831cd2f7bba4e37c22efb4b21b57bab2a36b9e8e3eccd57db3c75114ba0f788013a08f404738535e9a7eb8a29a30592095e5347e446cf61d50d5508a624934584e17a436ba&event_collection=Loaded%20own%20profile&filters=%5B%7B%22property_name%22%3A%22keen.timestamp%22%2C%22operator%22%3A%22lt%22%2C%22property_value%22%3A%222013%2F07%2F17%22%7D%2C%7B%22property_name%22%3A%22keen.timestamp%22%2C%22operator%22%3A%22gt%22%2C%22property_value%22%3A%222013%2F06%2F17%22%7D%5D&target_property=user.userId"
     active_accounts_query_pattern = "https://api.keen.io/3.0/projects/51d858213843314922000002/queries/count_unique?api_key=69023dd079bdb913522954c0f9bb010766be7e87a543674f8ee5d3a66e9b127f5ee641546858bf2c260af4831cd2f7bba4e37c22efb4b21b57bab2a36b9e8e3eccd57db3c75114ba0f788013a08f404738535e9a7eb8a29a30592095e5347e446cf61d50d5508a624934584e17a436ba&event_collection=Loaded%20own%20profile&filters=%5B%7B%22property_name%22%3A%22keen.created_at%22%2C%22operator%22%3A%22lt%22%2C%22property_value%22%3A%22{from_date}%22%7D%2C%7B%22property_name%22%3A%22keen.created_at%22%2C%22operator%22%3A%22gte%22%2C%22property_value%22%3A%22{to_date}%22%7D%5D&target_property=user.userId"
+    max_cache_duration = 24*60*60 # one day
+    cache_client = cache.Cache(max_cache_duration)
 
     def format_date(self, date):
         date_only = date.isoformat()[0:10]
-        #escaped_date = date_only.replace("-", "%2F")
         return date_only
 
     def get_raw_data(self, number_of_bins):
@@ -272,11 +273,19 @@ class Keen(Widget):
             from_date = iso8601.parse_date(date)
             to_date = from_date - timedelta(days=30)
 
-            active_accounts_query_url = self.active_accounts_query_pattern.format(
-                from_date = self.format_date(from_date), 
-                to_date = self.format_date(to_date))
+            # get it from the cache if it is there
+            cache_key = "Keen_active_accounts_{from_date}_{to_date}".format(
+                from_date=from_date, 
+                to_date=to_date)
 
-            active_accounts = get_raw_keenio_data(active_accounts_query_url)
+            active_accounts = self.cache_client.get_cache_entry(cache_key)
+            if active_accounts is None:  
+                active_accounts_query_url = self.active_accounts_query_pattern.format(
+                    from_date = self.format_date(from_date), 
+                    to_date = self.format_date(to_date))
+                active_accounts = get_raw_keenio_data(active_accounts_query_url)
+                self.cache_client.set_cache_entry(cache_key, active_accounts)
+
             fraction_active = (0.0+int(active_accounts)) / int(total_accounts)
 
             data["timestamp_list"].append(int(time.mktime(from_date.timetuple())))
@@ -288,7 +297,7 @@ class Keen(Widget):
 
 
     def get_data(self):
-        number_of_bins = 7  # eventually make this 30 days
+        number_of_bins = 7  # eventually make this bins for 30 days
         data = self.get_raw_data(number_of_bins)
 
         data["timestamp_list"].reverse()
