a5a7a24a57bb03a90f20f043cfce48433c76083d
Jason Priem <jp@jasonpriem.org>
2013-07-23 21:49:17 -0700

    Git commit embedded_widget_use.js widget sorta works; close enough for bed anyway.

--- impactstoryanalytics/static/js_widgets/embedded_widget_use.js
+++ impactstoryanalytics/static/js_widgets/embedded_widget_use.js
@@ -4,25 +4,42 @@ var Embedded_widget_use = function() {
 Embedded_widget_use.prototype = {
     init: function(){
     }
-    ,create: function(data){
+    ,create:function(data){
         console.log("creating embedded_widget_use widget")
         console.log(data)
-    }
-    ,createOld:function(data){
-        var overallMax = 0
-        for (name in data) {
-            var values =  _.pluck(data[name], "y")
-            overallMax = _.max([overallMax, _.max(values)])
-        }
+        var overallMax = this.findOverallMax(data)
+
         var ss = new SparklineSet(
-            $(".widget-gmail"),
+            $(".embedded-widget-use"),
             {chartRangeMax: overallMax}
         )
-
-        _.each(data, function(points, name) {
-            var xValues = _.pluck(points, "x" )
-            var yValues = _.pluck(points, "y" )
-            ss.createSparklineLine(name, xValues, yValues)
+        var xValues = _.map(_.pluck(data, "start_iso"), function(iso){
+            return moment(iso).format("X")
         })
+
+        var conversionRate = _.map(data, function(row){
+            if (!row.pageviews) {
+                return null
+            }
+            else {
+                return Math.round(100 * row.clickthroughs / row.pageviews)
+            }
+        })
+        console.log("conversion rate", conversionRate)
+
+
+        ss.createSparklineLine("clickthroughs", xValues, _.pluck(data, "clickthroughs"))
+        ss.createSparklineLine("pageviews", xValues, _.pluck(data, "pageviews"))
+        ss.options.iaPrimaryUnit = "%"
+        ss.createSparklineLine("conversion-rate", xValues, conversionRate)
+
+    }
+    ,findOverallMax: function(data){
+        var overallMax = 0
+        for (var timeslice in data) {
+            var thisTimeSliceMax = _.max([timeslice.clickthroughs, timeslice.pageviews])
+            overallMax = _.max([overallMax, thisTimeSliceMax])
+        }
+        return overallMax
     }
 }
