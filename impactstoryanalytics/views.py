746a5b80570fe2306c39614d80ba278120abdf78
Heather Piwowar <hpiwowar@gmail.com>
2013-07-23 20:10:03 -0700

    clean out old api cruft and add placeholder webhook handler

--- impactstoryanalytics/views.py
+++ impactstoryanalytics/views.py
@@ -94,90 +94,6 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/inbox-count')
-def inbox_count():
-    count = gmail.count_threads_in_inbox(
-        os.getenv("GMAIL_CLIENT_ID"),
-        os.getenv("GMAIL_CLIENT_SECRET"),
-        os.getenv("GMAIL_REFRESH_TOKEN_HEATHER"),
-        os.getenv("GMAIL_ADDRESS_HEATHER")
-    )
-
-    return make_response(str(count))
-
-
-
-@app.route('/active-users')
-def active_users():
-    active_users_keenio_query = "https://api.keen.io/3.0/projects/51d858213843314922000002/queries/count_unique?api_key=69023dd079bdb913522954c0f9bb010766be7e87a543674f8ee5d3a66e9b127f5ee641546858bf2c260af4831cd2f7bba4e37c22efb4b21b57bab2a36b9e8e3eccd57db3c75114ba0f788013a08f404738535e9a7eb8a29a30592095e5347e446cf61d50d5508a624934584e17a436ba&event_collection=Viewed%20own%20profile&filters=%5B%7B%22property_name%22%3A%22days_since_account_created%22%2C%22operator%22%3A%22gte%22%2C%22property_value%22%3A1%7D%5D&timeframe=this_week&timezone=US%2FPacific&target_property=user.traits.email&interval=daily"
-
-    keenio_data = requests.get(active_users_keenio_query).json()["result"]
-    timepoints = [datapoint["timeframe"]["start"] for datapoint in keenio_data]
-    values = [datapoint["value"] for datapoint in keenio_data]
-
-    date_format = "%b %d"  # Mar 10
-    min_timepoint = iso8601.parse_date(timepoints[0])
-    max_timepoint = iso8601.parse_date(timepoints[len(timepoints)-1])
-
-    gecko_response = {
-        "item": values,
-        "settings": {
-         "axisx": [min_timepoint.strftime(date_format), max_timepoint.strftime(date_format)],
-         "axisy": [min(values), max(values)],
-         "colour": "ff9900"
-        }
-    }
-
-    resp = make_response(json.dumps(gecko_response, indent=4), 200)
-    resp.mimetype = "application/json"
-    return resp
-
-
-
-
-@app.route("/rescuetime/<first_name>")
-def rescuetime_endpoint(first_name):
-    data = rescuetime.get_data(first_name)
-    dayslist = rescuetime.list_activity_by_day(data)
-
-    chart = highcharts.streamgraph()
-    chart["xAxis"] = {
-        "categories": [day["name"] for day in dayslist]
-    }
-    chart["yAxis"]["max"] = 15
-    colors = [  # these will stack in this same order on the graph
-        ("other", "#666666"),
-        ("email", "#D7191C"),
-        ("code", "#1A9641")
-    ]
-    for series_name, color in colors:
-        this_series = {
-            "data": [round(day[series_name], 1) for day in dayslist],
-            "name": series_name,
-            "color": color
-        }
-        chart["series"].append(this_series)
-
-    resp = make_response(highcharts.as_js(chart), 200)
-    resp.mimetype = "application/x-javascript"
-
-    return resp
-
-
-@app.route("/uservoice-tickets")
-def uservoice_tickets():
-    from impactstoryanalytics import uservoice_check
-    (num_all_tickets, num_last_response_was_a_user) = uservoice_check.get_ticket_counts()
-
-    gecko_response = {"item":[
-        {"text":"","value":num_all_tickets},
-        {"text":"","value":num_last_response_was_a_user}]}
-
-    resp = make_response(json.dumps(gecko_response, indent=4), 200)
-    resp.mimetype = "application/json"
-    return resp
-
-
 @app.route("/widget_data/<widget_name>")
 def widget_data(widget_name):
     module = sys.modules["impactstoryanalytics.widgets." + widget_name.lower()]  # hack, ick
@@ -185,6 +101,13 @@ def widget_data(widget_name):
     widget = getattr(module, class_name)()
 
     resp = make_response(json.dumps(widget.get_data(), indent=4), 200)
+    resp.mimetype = "application/json"
+    return resp
+
+
+@app.route("/webhook/<source>", methods=['POST'])
+def webhook(source):
+    resp = make_response(json.dumps({"source": source}, indent=4), 200)
     resp.mimetype = "application/json"
     return resp
 
