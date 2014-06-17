d48b8c57c8956df6d62a48bae454333367d21d26
Heather Piwowar <hpiwowar@gmail.com>
2014-06-16 18:07:53 -0700

    show numbers in all queues

--- impactstoryanalytics/widgets/celery.py
+++ impactstoryanalytics/widgets/celery.py
@@ -11,20 +11,28 @@ from impactstoryanalytics.widgets.widget_api_helpers import Keenio
 
 logger = logging.getLogger("impactstoryanalytics.widgets.celery")
 
-celery_redis_database_number = 1
-my_celery_redis = redis.from_url(os.getenv("REDIS_URL"), celery_redis_database_number)
+REDIS_CELERY_TASKS_DATABASE_NUMBER = 1
+my_celery_redis = redis.from_url(os.getenv("REDIS_URL"), REDIS_CELERY_TASKS_DATABASE_NUMBER)
 
 class Celery(Widget):
 
     def get_data(self):
         response = []
-        for queue_name in ["core_main", "celery"]:
-            queue_length = my_celery_redis.llen(queue_name)
-            if queue_length > 100:
-                logger.warning(u"HIGH celery queue length: {queue_name} is length {queue_length}".format(
-                    queue_name=queue_name, queue_length=queue_length))
 
-            response.append({"queue_name":queue_name, "queue_length":queue_length})
+        #[(q, len(r_tasks.lrange(q, -10000, 10000))) for q in r_tasks.keys("core_*")]
+
+        for queue_name_base in ["core_*", "celery"]:
+            queue_length_total = 0
+            for queue_name in my_celery_redis.keys(queue_name_base):
+                queue_length = my_celery_redis.llen(queue_name)
+                queue_length_total += queue_length
+                if queue_length > 100:
+                    logger.warning(u"HIGH celery queue length: {queue_name} is length {queue_length}".format(
+                        queue_name=queue_name, queue_length=queue_length))
+
+                response.append({"queue_name":queue_name, "queue_length":queue_length})
+
+            response.append({"queue_name":queue_name_base+"TOTAL", "queue_length":queue_length_total})
             
         logger.info(u"celery queue length: {response}".format(
             response=response))
